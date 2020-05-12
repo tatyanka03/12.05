b6c655582775564f843627807a066ce1a9010d6f
tatyanka03 <63300782+tatyanka03@users.noreply.github.com>
2020-05-12 16:18:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { };
+            while (true)
+            {
+                Console.WriteLine("Введите имя ");
+                string name = Console.ReadLine();
+
+                names.Add(name);
+
+                Console.WriteLine("Нажмите 1 чтобы продолжить 2 чтобы выйти");
+
+                if (Console.ReadLine() == "2")
+                { break; }
+
+            }
+            names.Sort();
+            names = names.Distinct().ToList();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine(" Всего имен" + names.Count);
+
+            Console.ReadLine();
+                
+        }
+    }
+}
